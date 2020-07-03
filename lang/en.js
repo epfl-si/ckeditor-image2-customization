e4bfebf93c6c208cc9143aab3be195fefeb34ea4
William Belle <william.belle@gmail.com>
2020-07-03 15:16:03 +0200

    Add custom i18n

---
+++ lang/en.js
@@ -0,0 +1,20 @@
+/*
+ * (c) ECOLE POLYTECHNIQUE FEDERALE DE LAUSANNE, Switzerland, VPSI, 2020.
+ */
+
+CKEDITOR.plugins.setLang('image2', 'en', {
+  alt: 'Alternative Text',
+  btnUpload: 'Send it to the Server',
+  captioned: 'Display caption',
+  captionPlaceholder: '© ' + (new Date().getFullYear()).toString() + ' EPFL',
+  infoTab: 'Image Info',
+  lockRatio: 'Lock Ratio',
+  menu: 'Image Properties',
+  pathName: 'image',
+  pathNameCaption: 'caption',
+  resetSize: 'Reset Size',
+  resizer: 'Click and drag to resize',
+  title: 'Image Properties',
+  uploadTab: 'Upload',
+  urlMissing: 'Image source URL is missing.'
+});
---
+++ lang/fr.js
@@ -0,0 +1,20 @@
+/*
+ * (c) ECOLE POLYTECHNIQUE FEDERALE DE LAUSANNE, Switzerland, VPSI, 2020.
+ */
+
+CKEDITOR.plugins.setLang('image2', 'fr', {
+  alt: 'Texte de remplacement',
+  btnUpload: 'Envoyer sur le serveur',
+  captioned: 'Afficher une légende',
+  captionPlaceholder: '© ' + (new Date().getFullYear()).toString() + ' EPFL',
+  infoTab: 'Informations sur l\'image',
+  lockRatio: 'Conserver les proportions',
+  menu: 'Propriétés de l\'image',
+  pathName: 'image',
+  pathNameCaption: 'légende',
+  resetSize: 'Taille d\'origine',
+  resizer: 'Cliquer et glisser pour redimensionner',
+  title: 'Propriétés de l\'image',
+  uploadTab: 'Envoyer',
+  urlMissing: 'L\'adresse source de l\'image est manquante.'
+});
