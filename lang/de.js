13fcbeb60d1c72641439a54cb7426a21634a17ba
Lindo Duratti <lindo.duratti@epfl.ch>
2020-11-26 11:19:50 +0100

    Add German language (#2)

---
+++ lang/de.js
@@ -0,0 +1,20 @@
+/*
+ * (c) ECOLE POLYTECHNIQUE FEDERALE DE LAUSANNE, Switzerland, VPSI, 2020.
+ */
+
+CKEDITOR.plugins.setLang('image2', 'de', {
+  alt: 'Alternativer Text',
+  btnUpload: 'Zum Server senden',
+  captioned: 'Bild mit Überschrift',
+  captionPlaceholder: '© ' + (new Date().getFullYear()).toString() + ' EPFL',
+  infoTab: 'Bildinfo',
+  lockRatio: 'Größenverhältnis beibehalten',
+  menu: 'Bildeigenschaften',
+  pathName: 'Bild',
+  pathNameCaption: 'Überschrift',
+  resetSize: 'Größe zurücksetzen',
+  resizer: 'Zum Vergrößern auswählen und ziehen',
+  title: 'Bild-Eigenschaften',
+  uploadTab: 'Hochladen',
+  urlMissing: 'Bildquellen-URL fehlt.'
+});
